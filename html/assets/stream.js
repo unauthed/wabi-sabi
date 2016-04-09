43da2580ca1c6aed1375cf75ecc6975855a4e930
urchinly <git@urchinly.uk>
2016-04-09 20:20:29 +0100

    Add stream events page.

--- html/assets/stream.js
+++ html/assets/stream.js
@@ -1,66 +1,24 @@
 
 $(document).ready(function() {
 
-  var services = null;
-  var wabiServicesData = [];
-  var sabiServicesData = [];
-  var intervalDuration = 30000;
-  var intervalId = setInterval(updateStatus, intervalDuration);
+  var events = [];
+  var template = $.templates("#eventTemplate");
+  template.link("#eventsContainer", events);
 
-  var template = $.templates("#servicesTemplate");
-  template.link("#wabiServicesContainer", wabiServicesData);
-  template.link("#sabiServicesContainer", sabiServicesData);
+  function connect() {
+    var socket = new SockJS("//" + location.hostname + ':7081/stomp');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function(frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/events', function(event){
+          if(events.length > 9) {
+            $.observable(events).remove(0);
+          }
 
-  function updateServiceStatus (service, serviceData) {
-    $.ajax({
-        url: "//" + location.hostname + ":" + service.port + "/info"
-    })
-    .done(function(data) {
-      data.status = "UP";
-      $.observable(serviceData).insert(data);
-    })
-    .fail(function() {
-      $.observable(serviceData).insert({"build": {"name": service.name, "description": "Not responding...", "status": "DOWN"}});
-    })
+          $.observable(events).insert(JSON.parse(event.body));
+        });
+    });
   }
 
-  function updateWabiStatus() {
-    for (service of services.wabiServices) {
-      $.observable(wabiServicesData).remove(0);
-      updateServiceStatus(service, wabiServicesData);
-    }
-
-    console.log(wabiServicesData);
-  }
-
-  function updateSabiStatus() {
-    for (service of services.sabiServices) {
-      $.observable(sabiServicesData).remove(0);
-      updateServiceStatus(service, sabiServicesData);
-    }
-
-    console.log(sabiServicesData);
-  }
-
-  function updateStatus () {
-    updateWabiStatus();
-    updateSabiStatus();
-  }
-
-  $('.closeall').click(function(){
-    $('.panel-collapse.in')
-      .collapse('hide');
-  });
-
-  $('.openall').click(function(){
-    $('.panel-collapse:not(".in")')
-      .collapse('show');
-  });
-
-  $.getJSON("status.json", function(json) {
-    console.log(json);
-    services = json;
-    updateStatus();
-  });
-
+  connect();
 });
