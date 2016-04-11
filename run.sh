de80acbf38b3db25ba1302adf124d5c168f82da0
urchinly <git@urchinly.uk>
2016-04-11 21:23:21 +0100

    Make this the parent repo and use git modules to pull in the other repos.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# docker run -it -p 9080:80 --memory 500m --rm --net=wabi_sabi-tier --name sabi-nginx urchinly/sabi-nginx
-
-docker run -it -p 9080:80 -v $PWD/html:/usr/share/nginx/html --memory 500m --rm --net=wabi_sabi-tier --name sabi-nginx urchinly/sabi-nginx
-
